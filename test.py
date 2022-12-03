0211ff7c9576a52bc605b19faf87741c3ddbceba
MEGADragon2.0 <106551997+MEGADragon20@users.noreply.github.com>
2022-12-03 12:10:11 +0100

    h

--- test.py
+++ test.py
@@ -1,11 +1,7 @@
-class Field:
-    def __init__ (self, eins: str, zwei: str):
-        self.eins = eins
-        self.zwei = zwei
-
 class Person:
-    def __init__ (self, ID: int = None, field_1: Field = None):
+    def __init__ (self, ID: int = None, field_1: str = None):
         self.ID = ID
         self.field_1 = field_1
+    
 
 
